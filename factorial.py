152fd59f3e16f585d83de86098d2758786c77ce7
Harris <moejoe494@gmail.com>
2025-10-03 18:07:06 -0400

    update 1

--- factorial.py
+++ factorial.py
@@ -8,12 +8,12 @@ def factorial(n):
 
     time.sleep(.1)
 
-    factorial = 1
+    fac = 1
 
     for i in range (1,n+1):
-        factorial = factorial * i
+        fac = fac * i
 
-    return factorial
+    return fac
 
 def sum_factorial():
 
